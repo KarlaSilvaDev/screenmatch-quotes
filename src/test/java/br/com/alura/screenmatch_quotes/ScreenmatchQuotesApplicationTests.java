955153f5d8553d15c78741a7883e8273dedbe16a
Karlasilvaeng <karlasilvaeng@gmail.com>
2024-07-03 20:28:47 -0300

    First commit

---
+++ src/test/java/br/com/alura/screenmatch_quotes/ScreenmatchQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.alura.screenmatch_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
